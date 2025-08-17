5b40858a6d3981cccaaaf572b231bcdcca6c0b4a
Soyeon <soye0200@gmail.com>
2025-08-18 02:20:00 +0900

    ✨ feat: Send data to model fastAPI server
    데이터 수신 후 이벤트 발행 시 모델 서버로 데이터 보냄(/predict)

---
+++ pressfaultdetection/src/main/java/carsmartfactory/infra/dto/PressFaultPredictionRequestDto.java
@@ -0,0 +1,17 @@
+package carsmartfactory.infra.dto;
+
+import lombok.Data;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Data
+public class PressFaultPredictionRequestDto {
+    @JsonProperty("AI0_Vibration")
+    private List<Double> AI0_Vibration;
+    
+    @JsonProperty("AI1_Vibration")
+    private List<Double> AI1_Vibration;
+    
+    @JsonProperty("AI2_Current")
+    private List<Double> AI2_Current;
+}
