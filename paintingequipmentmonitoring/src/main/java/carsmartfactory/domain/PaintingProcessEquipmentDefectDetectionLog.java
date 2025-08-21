330ed326e35dcaaad2f20c5882533614d62528d0
TaeHyun aivle <xogus607@naver.com>
2025-08-21 02:01:48 +0000

    🚚 Fix: move ecoating project folders
    paintingprocessmonitoring 폴더 내 전착도장 설비 시스템을 분리해 paintingequipmentmonitoring 폴더로 옮김

---
+++ paintingequipmentmonitoring/src/main/java/carsmartfactory/domain/PaintingProcessEquipmentDefectDetectionLog.java
@@ -0,0 +1,87 @@
+package carsmartfactory.domain;
+
+import carsmartfactory.PaintingprocessmonitoringApplication;
+import carsmartfactory.domain.EquipmentIssueSolved;
+import carsmartfactory.domain.PaintingProcessEquipmentDefectSaved;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+// javax → jakarta 패키지 변경
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "PaintingProcessEquipmentDefectDetectionLog_table")
+@Data
+//<<< DDD / Aggregate Root
+public class PaintingProcessEquipmentDefectDetectionLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String machineId;
+
+    private Date timeStamp;
+
+    private Float thick;
+
+    private Float voltage;
+
+    private Float current;
+
+    private Float temper;
+
+    private String issue;
+
+    private Boolean isSolved;
+
+    @PostPersist
+    public void onPostPersist() {
+        PaintingProcessEquipmentDefectSaved paintingProcessEquipmentDefectSaved = new PaintingProcessEquipmentDefectSaved(
+                this
+        );
+        paintingProcessEquipmentDefectSaved.publishAfterCommit();
+    }
+
+    public static PaintingProcessEquipmentDefectDetectionLogRepository repository() {
+        PaintingProcessEquipmentDefectDetectionLogRepository paintingProcessEquipmentDefectDetectionLogRepository = PaintingprocessmonitoringApplication.applicationContext.getBean(
+                PaintingProcessEquipmentDefectDetectionLogRepository.class
+        );
+        return paintingProcessEquipmentDefectDetectionLogRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void equipmentIssueSolvedPolicy(IssueSolved issueSolved) {
+        //implement business logic here:
+
+        /** Example 1:  new item
+         PaintingProcessEquipmentDefectDetectionLog paintingProcessEquipmentDefectDetectionLog = new PaintingProcessEquipmentDefectDetectionLog();
+         repository().save(paintingProcessEquipmentDefectDetectionLog);
+
+         EquipmentIssueSolved equipmentIssueSolved = new EquipmentIssueSolved(paintingProcessEquipmentDefectDetectionLog);
+         equipmentIssueSolved.publishAfterCommit();
+         */
+
+        /** Example 2:  finding and process
+
+
+         repository().findById(issueSolved.get???()).ifPresent(paintingProcessEquipmentDefectDetectionLog->{
+
+         paintingProcessEquipmentDefectDetectionLog // do something
+         repository().save(paintingProcessEquipmentDefectDetectionLog);
+
+         EquipmentIssueSolved equipmentIssueSolved = new EquipmentIssueSolved(paintingProcessEquipmentDefectDetectionLog);
+         equipmentIssueSolved.publishAfterCommit();
+
+         });
+         */
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
