68b0f63320346e00bd7a95a38f04dcf19d0e60c5
SoYeon <soye0200@gmail.com>
2025-08-24 23:53:25 +0900

    ✨ feat: create GET /posts api
    - 게시글 목록 조회 api 생성

--- report/src/main/java/carsmartfactory/infra/PostController.java
+++ report/src/main/java/carsmartfactory/infra/PostController.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,11 @@ public class PostController {
 
     @Autowired
     PostRepository postRepository;
+
+    @GetMapping("/posts")
+    public ResponseEntity<Iterable<Post>> getAllPosts() {
+        Iterable<Post> posts = postRepository.findAll();
+        return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
 }
 //>>> Clean Arch / Inbound Adaptor
