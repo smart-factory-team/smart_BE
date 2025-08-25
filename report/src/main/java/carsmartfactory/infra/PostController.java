e542f55b7a50268850c4db074c1d9882b436acf9
SoYeon <soye0200@gmail.com>
2025-08-25 09:44:10 +0900

    ♻️ refactor: modify PostController
    - 코드 주석 처리

--- report/src/main/java/carsmartfactory/infra/PostController.java
+++ report/src/main/java/carsmartfactory/infra/PostController.java
@@ -1,6 +1,7 @@
 package carsmartfactory.infra;
 
 import carsmartfactory.domain.*;
+import java.util.Map;
 import java.util.Optional;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -22,10 +23,42 @@ public class PostController {
     @Autowired
     PostRepository postRepository;
 
-    @GetMapping("/posts")
-    public ResponseEntity<Iterable<Post>> getAllPosts() {
-        Iterable<Post> posts = postRepository.findAll();
-        return new ResponseEntity<>(posts, HttpStatus.OK);
-    }
+    // @GetMapping("/posts")
+    // public ResponseEntity<Iterable<Post>> getAllPosts() {
+    //     Iterable<Post> posts = postRepository.findAll();
+    //     return new ResponseEntity<>(posts, HttpStatus.OK);
+    // }
+
+    // @PostMapping("/posts")
+    // public ResponseEntity<Post> createPost(@RequestBody Post post) {
+    //     Post savedPost = postRepository.save(post);
+    //     return new ResponseEntity<>(savedPost, HttpStatus.CREATED);
+    // }
+
+    // @PatchMapping("/posts/{id}")
+    // public ResponseEntity<Post> updatePost(@PathVariable String id, @RequestBody Map<String, Object> updates) {
+    //     return postRepository.findById(id).map(post -> {
+    //         updates.forEach((key, value) -> {
+    //             if (key.equals("title")) {
+    //                 post.setTitle((String) value);
+    //             }
+    //             if (key.equals("content")) {
+    //                 post.setContent((String) value);
+    //             }
+    //         });
+    //         Post updatedPost = postRepository.save(post);
+    //         return new ResponseEntity<>(updatedPost, HttpStatus.OK);
+    //     }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    // }
+
+    // @DeleteMapping("/posts/{id}")
+    // public ResponseEntity<Void> deletePost(@PathVariable String id) {
+    //     if (postRepository.existsById(id)) {
+    //         postRepository.deleteById(id);
+    //         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    //     } else {
+    //         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    //     }
+    // }
 }
 //>>> Clean Arch / Inbound Adaptor
