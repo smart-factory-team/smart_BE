2eeef32cc3d1ff830eca32699c7dc734f95c57a2
haeyekim aivler <godus0613@naver.com>
2025-08-27 20:36:40 +0000

    ⚙️ chore: Add RestTemplate as a Spring Bean
    외부 API 호출을 위해 RestTemplate 빈을 등록. PressDefectDetectionController에서 모델 서비스와 통신하는 데 사용됨.

--- pressdefectmonitoring/src/main/java/carsmartfactory/PressDefectMonitoringApplication.java
+++ pressdefectmonitoring/src/main/java/carsmartfactory/PressDefectMonitoringApplication.java
@@ -2,11 +2,13 @@ package carsmartfactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PressDefectMonitoringApplication {
     
     public static ApplicationContext applicationContext;
